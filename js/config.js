a5c38b3aa3391256ab2089d6be115bae9295a70e
nabaybuilding-design <nabaybuilding@gmail.com>
2026-04-16 16:37:36 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,6 @@
 window.APP_CONFIG = {
   appName: "Chartouni_Attalah_Building",
-
-  // Local mode for now
   useSupabase: true,
-
-  // Fill these later
   supabaseUrl: "https://kmsrncvypfhlxnysgacm.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttc3JuY3Z5cGZobHhueXNnYWNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNzE0MTEsImV4cCI6MjA5MTg0NzQxMX0.9F-mvG3ZX9vRvqtBY433lCWNRx6W1PLY4qJA8pVfANU"
 };
