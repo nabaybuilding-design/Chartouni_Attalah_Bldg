9a55b2fd77035736043d62faa139bdbc539b6aa6
nabaybuilding-design <nabaybuilding@gmail.com>
2026-04-16 04:13:23 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window.APP_CONFIG = {
-  appName: "Chartouni_Attalah_Building",
+  appName: "Chartouni_Attalah_Bldg",
 
   // Local mode for now
   useSupabase: false,
 
   // Fill these later
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://avpbqiljlgbyhabkbxle.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2cGJxaWxqbGdieWhhYmtieGxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NTMyODUsImV4cCI6MjA5MTQyOTI4NX0.3A1pb9gwZBH8C_WJI17Kksx9w8ZTY-r7zTvH_4Cmmlk"
 };
